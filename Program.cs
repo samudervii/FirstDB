64135c158a111bb809809224969c10112a736989
Samuele Dervishi <117899052+samudervii@users.noreply.github.com>
2023-05-04 10:11:06 +0200

    prima versione

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿﻿using SQLite;
+
+Console.WriteLine("Hello World");
+
+SQLiteConnection cnl = new SQLiteConnection("chinook.db");
+var tblArtist = cnl.Query<Artist>("select * from artists");
+Console.WriteLine( $"In questa tabella ci sono {tblArtist.Count} record!");
+
+public class Artist
+{
+    public int ArtistId{get; set;}
+    public string Name{get; set;}
+}
